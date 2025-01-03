6582500826d1d2acc3108592f2ef26de7e04751e
Anya Parekh <anzparekh@gmail.com>
2025-01-02 20:34:38 -0600

    potential fix for vercel build

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import vercel from 'vite-plugin-vercel';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    target: 'esnext'
-  },
-  plugins: [react(), vercel()],
-  server: {
-    port: 3000,
-    strictPort: true,
-    host: true,
-  }
+  plugins: [react()],
 })
